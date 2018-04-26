a89da2d36fc44bddf73d2ee35cfc66859d3e0846
Omar <ocontreras.sf@gmail.com>
2018-04-26 16:55:09 +0000

    Account for blank lines in monty file(#1)

--- string_helpers.c
+++ string_helpers.c
@@ -16,6 +16,8 @@ char * get_tokens(char *line, unsigned int line_number)
 	printf("line number %d\n", (int)line_number);
 	token = strtok(line, delim);
 	printf("first token: %s\n", token);
+	if (token == NULL)
+		return (NULL);
 	global_var = atoi(strtok(NULL, delim));
 	printf("second token (global var) %d\n", global_var);
 	return (token);
