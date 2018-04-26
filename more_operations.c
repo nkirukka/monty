fdfd6b897037ef1ad51a60d59715fd8682a86e96
kevanlucc <kevan.lucchini@gmail.com>
2018-04-26 22:39:26 +0000

    add more functions

---
+++ more_operations.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop_func - implement the nop function
+ * @stack: stack
+ * @line_number: line number
+ */
+void nop_func(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+
+	printf("NOOOOP");
+}
